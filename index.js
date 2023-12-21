01b0b35361ee14cd9398073475a947685a1dcf8d
Tiranga Sheetal Beck <doordion@gmail.com>
2023-12-21 15:26:47 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const db = require("./data/db.json");
+const app = express();
+const PORT = 3000;
+const fs = require('fs');
+
+// Middleware
+app.use(express.urlencoded({extended: false}));
+
+// REST API
+app.get('/api/data', (req, res) => {
+    return res.json(db)
+})
+
+app.get('/api/data/:id', (req, res) => {
+    const id = Number(req.params.id);
+    // Find ID in db
+    const findID = db.find((findID) => findID.id === id);
+    return res.json(findID)
+})
+
+app.post('/api/data', (req, res) =>{
+    const body = req.body;
+    db.push({...body, id: Date.now()});
+    fs.writeFile('./data/db.json', JSON.stringify(db), (err, data) => {
+        return res.json({status: "Successfully Inserted"});
+    })
+})
+
+app.delete('/api/data/:id', (req, res) =>{
+    const id = Number(req.params.id);
+    // Find ID in db
+    const findID = db.find((findID) => findID.id === id);
+    db.splice(findID, 1)
+    return res.json({status: "Successfully Deleted"});
+})
+
+app.listen(PORT, () => console.log('Server started at PORT 8000'));
